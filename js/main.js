e54f75460a8d47afbe523f563e855ae7275d3783
Nicola-Campagna <campagnanicola379@gmail.com>
2023-01-23 13:33:11 +0100

    add sistem password generator

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@
 // poi chiedi il suo colore preferito,
 // infine scrivi sulla pagina nomecognomecolorepreferito23
 
+// PASSAGGI:
+
+// chiedere nome utente
+const userName = prompt("Inserisci Nome");
+console.log(userName);
+
+// chiedere cognome utente
+const userSurname = prompt("Inserisci Cognome");
+console.log(userSurname);
+
+// chiedere colore preferito utente
+const favColor = prompt("Inserisci colore preferito");
+console.log(favColor);
+
+// generare password visibile in pagina
+const userPwd = (userName + userSurname + favColor + "23");
+console.log("User Password", userPwd);
+document.getElementById("Pwd").innerHTML = ("Password generate = " + userPwd);
